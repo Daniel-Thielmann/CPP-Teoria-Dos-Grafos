cf6f3cd41c20a22407634ee256f231f91db0f4a8
Daniel Thielmann <daniel.thielmann@example.com>
2024-12-29 21:36:33 -0300

    get_grau

--- funcoes/get_grau.cpp
+++ funcoes/get_grau.cpp
@@ -2,6 +2,11 @@
 
 // Calcula o grau de um vértice específico
 int get_grau(const std::vector<std::vector<int>>& adjacencias, int vertice) {
+    // Verifica se o índice do vértice está dentro dos limites
+    if (vertice < 0 || vertice >= adjacencias.size()) {
+        return -1; // Retorna -1 para indicar um erro
+    }
 
-    return 0;
+    // O grau do vértice é o número de arestas conectadas a ele
+    return adjacencias[vertice].size();
 }
